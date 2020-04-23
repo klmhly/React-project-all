1423e464f2a41576559704e020bcfd01751a04b1
xiaoyu.jia <xiaoyu.jia@za.group>
2020-04-23 20:29:58 +0800

    fix: 使用url-loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -289,8 +289,6 @@ module.exports = function(webpackEnv) {
         .map(ext => `.${ext}`)
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
-        // Support React Native Web
-        // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
@@ -300,15 +298,10 @@ module.exports = function(webpackEnv) {
         ...(modules.webpackAliases || {}),
       },
       plugins: [
-        // Adds support for installing with Plug'n'Play, leading to faster installs and adding
-        // guards against forgotten dependencies and such.
         PnpWebpackPlugin,
-        // Prevents users from importing files from outside of src/ (or node_modules/).
-        // This often causes confusion because we only process files within src/ with babel.
-        // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
-        // please link the files into your node_modules/ and let module-resolution kick in.
-        // Make sure your source files are compiled, as they will not be processed in any way.
-        new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+        
+        //jxy1: 注释掉 ModuleScopePlugin，src引入目录外部文件
+        // new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),     
       ],
     },
     resolveLoader: {
@@ -352,7 +345,7 @@ module.exports = function(webpackEnv) {
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
             {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],     // 图片用require引入，小于limit大小的会被转为base64
               loader: require.resolve('url-loader'),
               options: {
                 limit: imageInlineSizeLimit,
---
+++ src/components/imageToBase64/Index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+        <img src={require('../../../public/images/1.png')} />
+        <img src={require('../../../public/images/2.png')} />
+        <img src={require('../../../public/images/3.png')} />
+        <img src={require('../../../public/images/4.png')} />
+        <div>
+          <img src={require('../../../public/images/base64result.png')} />
+        </div>
+      </div>
+  );
+}
+
+export default App;
